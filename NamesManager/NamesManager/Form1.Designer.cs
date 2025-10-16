629eedfaf4ed4e47f7d9aa249f057c9dcfb26724
noa <noaa2152@gmail.com>
2025-10-16 17:13:45 +0300

    neat the pakadim

--- NamesManager/NamesManager/Form1.Designer.cs
+++ NamesManager/NamesManager/Form1.Designer.cs
@@ -34,11 +34,17 @@
             this.resolveName = new System.Windows.Forms.Button();
             this.addName = new System.Windows.Forms.Button();
             this.removeName = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // nameInput
             // 
-            this.nameInput.Location = new System.Drawing.Point(186, 69);
+            this.nameInput.Location = new System.Drawing.Point(20, 22);
             this.nameInput.Name = "nameInput";
             this.nameInput.Size = new System.Drawing.Size(100, 22);
             this.nameInput.TabIndex = 0;
@@ -46,7 +52,7 @@
             // name
             // 
             this.name.AutoSize = true;
-            this.name.Location = new System.Drawing.Point(183, 50);
+            this.name.Location = new System.Drawing.Point(17, 3);
             this.name.Name = "name";
             this.name.Size = new System.Drawing.Size(44, 16);
             this.name.TabIndex = 1;
@@ -54,16 +60,18 @@
             // 
             // namesLst
             // 
+            this.namesLst.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.namesLst.FormattingEnabled = true;
             this.namesLst.ItemHeight = 16;
-            this.namesLst.Location = new System.Drawing.Point(186, 114);
+            this.namesLst.Location = new System.Drawing.Point(277, 228);
             this.namesLst.Name = "namesLst";
-            this.namesLst.Size = new System.Drawing.Size(120, 84);
+            this.namesLst.Size = new System.Drawing.Size(120, 100);
             this.namesLst.TabIndex = 2;
             // 
             // resolveName
             // 
-            this.resolveName.Location = new System.Drawing.Point(353, 50);
+            this.resolveName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.resolveName.Location = new System.Drawing.Point(403, 199);
             this.resolveName.Name = "resolveName";
             this.resolveName.Size = new System.Drawing.Size(75, 23);
             this.resolveName.TabIndex = 3;
@@ -73,7 +81,7 @@
             // 
             // addName
             // 
-            this.addName.Location = new System.Drawing.Point(363, 114);
+            this.addName.Location = new System.Drawing.Point(3, 20);
             this.addName.Name = "addName";
             this.addName.Size = new System.Drawing.Size(75, 23);
             this.addName.TabIndex = 4;
@@ -83,7 +91,7 @@
             // 
             // removeName
             // 
-            this.removeName.Location = new System.Drawing.Point(363, 144);
+            this.removeName.Location = new System.Drawing.Point(3, 60);
             this.removeName.Name = "removeName";
             this.removeName.Size = new System.Drawing.Size(75, 23);
             this.removeName.TabIndex = 5;
@@ -91,23 +99,58 @@
             this.removeName.UseVisualStyleBackColor = true;
             this.removeName.Click += new System.EventHandler(this.removeName_Click);
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.resolveName, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.namesLst, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.removeName);
+            this.panel1.Controls.Add(this.addName);
+            this.panel1.Location = new System.Drawing.Point(403, 228);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(97, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.nameInput);
+            this.panel2.Controls.Add(this.name);
+            this.panel2.Location = new System.Drawing.Point(259, 170);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(138, 52);
+            this.panel2.TabIndex = 3;
+            // 
             // names
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.removeName);
-            this.Controls.Add(this.addName);
-            this.Controls.Add(this.resolveName);
-            this.Controls.Add(this.namesLst);
-            this.Controls.Add(this.name);
-            this.Controls.Add(this.nameInput);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "names";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.names_Load);
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.names_FormClosing);
+            this.Load += new System.EventHandler(this.names_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -119,6 +162,9 @@
         private System.Windows.Forms.Button resolveName;
         private System.Windows.Forms.Button addName;
         private System.Windows.Forms.Button removeName;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
 
--- NamesManager/NamesManager/Forms/FiltersForm.Designer.cs
+++ NamesManager/NamesManager/Forms/FiltersForm.Designer.cs
@@ -31,20 +31,25 @@
             this.filterNames = new System.Windows.Forms.ListBox();
             this.OK = new System.Windows.Forms.Button();
             this.Cancel = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // filterNames
             // 
+            this.filterNames.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.filterNames.FormattingEnabled = true;
             this.filterNames.ItemHeight = 16;
-            this.filterNames.Location = new System.Drawing.Point(268, 158);
+            this.filterNames.Location = new System.Drawing.Point(277, 183);
             this.filterNames.Name = "filterNames";
             this.filterNames.Size = new System.Drawing.Size(120, 84);
             this.filterNames.TabIndex = 0;
             // 
             // OK
             // 
-            this.OK.Location = new System.Drawing.Point(461, 158);
+            this.OK.Location = new System.Drawing.Point(15, 19);
             this.OK.Name = "OK";
             this.OK.Size = new System.Drawing.Size(75, 23);
             this.OK.TabIndex = 1;
@@ -54,23 +59,48 @@
             // 
             // Cancel
             // 
-            this.Cancel.Location = new System.Drawing.Point(461, 198);
+            this.Cancel.Location = new System.Drawing.Point(15, 59);
             this.Cancel.Name = "Cancel";
             this.Cancel.Size = new System.Drawing.Size(75, 23);
             this.Cancel.TabIndex = 2;
             this.Cancel.Text = "Cancel";
             this.Cancel.UseVisualStyleBackColor = true;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.filterNames, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.panel1.Controls.Add(this.OK);
+            this.panel1.Controls.Add(this.Cancel);
+            this.panel1.Location = new System.Drawing.Point(403, 175);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(103, 100);
+            this.panel1.TabIndex = 1;
+            // 
             // FiltersForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Cancel);
-            this.Controls.Add(this.OK);
-            this.Controls.Add(this.filterNames);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "FiltersForm";
             this.Text = "FiltersForm";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -80,5 +110,7 @@
         private System.Windows.Forms.ListBox filterNames;
         private System.Windows.Forms.Button OK;
         private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
